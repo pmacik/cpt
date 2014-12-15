28c3d3e4f01976d40198db69de8aa0125f64eb0c
Pavel Macík <pmacik@redhat.com>
2014-12-15 09:40:56 +0100

    Created a Maven project

---
+++ cpt-jetty/src/main/java/org/pmacik/cpt/jetty/CptBean.java
@@ -0,0 +1,9 @@
+package org.pmacik.cpt.jetty;
+public class CptBean {
+	public String process(String in) {
+		//String retVal = ;
+		//System.out.println(retVal);
+		//return retVal;
+		return "Hello " + in;
+	}
+}
