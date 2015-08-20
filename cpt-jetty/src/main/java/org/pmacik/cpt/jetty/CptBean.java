a897181fc2af7b7db71d213fdb4b832e986d0fa9
Pavel Macík <pmacik@redhat.com>
2015-08-20 15:01:28 +0200

    updated apps and scenarios

--- cpt-jetty/src/main/java/org/pmacik/cpt/jetty/CptBean.java
+++
@@ -1,9 +0,0 @@
-package org.pmacik.cpt.jetty;
-public class CptBean {
-	public String process(String in) {
-		//String retVal = ;
-		//System.out.println(retVal);
-		//return retVal;
-		return "Hello " + in;
-	}
-}
--- cpt-mqtt/src/main/java/org/pmacik/cpt/mqtt/CptBean.java
+++
@@ -1,7 +0,0 @@
-package org.pmacik.cpt.mqtt;
-public class CptBean {
-	public String process(String in) {
-		//System.out.println(in);
-		return "Hello " + in;
-	}
-}
