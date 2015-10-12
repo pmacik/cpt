73a5e3084c8385db6ed14de01b294f54b4c09bcb
Pavel Macík <pmacik@redhat.com>
2015-10-12 12:17:15 +0200

    [cpt] Formated XML files

--- cpt-mqtt-java/src/main/java/org/pmacik/cpt/mqtt/java/MqttRouteBuilder.java
+++ cpt-mqtt-java/src/main/java/org/pmacik/cpt/mqtt/java/MqttRouteBuilder.java
@@ -6,9 +6,14 @@ import org.pmacik.cpt.CptBean;
 public class MqttRouteBuilder extends RouteBuilder {
    @Override
    public void configure() throws Exception {
-      from("mqtt://mqtt-server?subscribeTopicName=" + System.getProperty(RuntimeProperties.REQUEST_TOPIC) + "&host=tcp://" + System.getProperty(RuntimeProperties.SERVER_HOST) + ":" + System.getProperty(RuntimeProperties.SERVER_PORT) + "/&qualityOfService=ExactlyOnce").bean(CptBean.class, "process")
-                                                                                                                                                                                                                                                                            .to("mqtt://mqtt-server?publishTopicName=" + System.getProperty(RuntimeProperties.RESPONSE_TOPIC) + "&host=tcp://" + System
-                                                                                                                                                                                                                                                                                  .getProperty(RuntimeProperties.SERVER_HOST) + ":" + System.getProperty(RuntimeProperties.SERVER_PORT)
-                                                                                                                                                                                                                                                                                  + "/&qualityOfService=ExactlyOnce");
+      from("mqtt://mqtt-server?subscribeTopicName="
+            + System.getProperty(RuntimeProperties.REQUEST_TOPIC)
+            + "&host=tcp://"
+            + System.getProperty(RuntimeProperties.SERVER_HOST) + ":" + System.getProperty(RuntimeProperties.SERVER_PORT)
+            + "/&qualityOfService=ExactlyOnce")
+            .bean(CptBean.class, "process")
+            .to("mqtt://mqtt-server?publishTopicName=" + System.getProperty(RuntimeProperties.RESPONSE_TOPIC) + "&host=tcp://"
+                  + System.getProperty(RuntimeProperties.SERVER_HOST) + ":" + System.getProperty(RuntimeProperties.SERVER_PORT)
+                  + "/&qualityOfService=ExactlyOnce");
    }
 }
--- cpt-mqtt-spring/src/main/java/org/pmacik/cpt/mqtt/spring/CptBean.java
+++ cpt-mqtt-spring/src/main/java/org/pmacik/cpt/mqtt/spring/CptBean.java
@@ -2,6 +2,7 @@ package org.pmacik.cpt.mqtt.spring;
 
 public class CptBean {
    public String process(String in) {
+      System.out.println("MQTT: ["+in+"]");
       return "Hello " + in;
    }
 }
